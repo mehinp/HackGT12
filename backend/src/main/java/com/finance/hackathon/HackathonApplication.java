a399e1be9a829dd8b0cadce0336072706c1d5564
mehinp <mehinpandya@gmail.com>
2025-09-26 22:29:45 -0400

    some edits

---
+++ backend/src/main/java/com/finance/hackathon/HackathonApplication.java
@@ -0,0 +1,13 @@
+package com.finance.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonApplication {
+
+    static void main(String[] args) {
+		SpringApplication.run(HackathonApplication.class, args);
+	}
+
+}
---
+++ backend/src/main/java/com/finance/hackathon/service/implementation/UserServiceImpl.java
@@ -0,0 +1,5 @@
+package com.finance.hackathon.service.implementation;
+
+public class UserServiceImpl {
+
+}
